d46b097f2bb23e78ed11f5f6ab5ecf9831825a95
Logan Lang <devloglogan@gmail.com>
2025-11-26 07:24:51 -0600

    Initialize OpenGL context

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <cstdlib>
+#include <iostream>
 
 #include "SDL3/SDL.h"
+#include "glad/glad.h"
 
 int main() {
 	SDL_Window *window;
 	bool done = false;
 
-	SDL_Init(SDL_INIT_VIDEO);
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 	window = SDL_CreateWindow("Learn OpenGL", 640, 480, SDL_WINDOW_OPENGL);
-
 	if (window == nullptr) {
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-		return EXIT_FAILURE;
+		std::cerr << "Could not create window: " << SDL_GetError() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	if (context == nullptr) {
+		std::cerr << "Failed to create context: " << SDL_GetError() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		exit(EXIT_FAILURE);
 	}
 
 	while (!done) {
@@ -30,5 +49,5 @@ int main() {
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
-	return EXIT_SUCCESS;
+	exit(EXIT_SUCCESS);
 }
